e3e247b80352ae74d822b555d7797205876c8d4a
Sujan Shrestha <aaryanlover4u@gmail.com>
2017-10-06 15:47:34 +0545

    before github

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,7 +15,7 @@
 
 		<div class="form-group">
 			{!! Form::label('category_id', 'Category:') !!}
-			{!! Form::select('category_id', array(''=>'Options', 1=>'Food', 2='Anime'), null, ['class'=>'form-control']) !!}
+			{!! Form::select('category_id', [''=>'Choose Category'] + $categories , null, ['class'=>'form-control']) !!}
 		</div>
 
 		<div class="form-group">
