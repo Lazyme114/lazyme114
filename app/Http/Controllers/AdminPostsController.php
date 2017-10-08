8ff76227521706db2aa83ec533b8f4ff525c353f
Sujan Shrestha <aaryanlover4u@gmail.com>
2017-10-08 12:49:41 +0545

    Category system working

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -84,6 +84,10 @@ class AdminPostsController extends Controller
     public function edit($id)
     {
         //
+
+        $post = Post::findOrFail($id);
+        $categories = Category::lists('name', 'id')->all();
+        return view('admin.posts.edit', compact('post', 'categories'));
     }
 
     /**
@@ -96,6 +100,20 @@ class AdminPostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')) {
+            $name = time().$file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['photo'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        Auth::user()->posts()->whereId($id)->first()->update($input);
+
+        return redirect('/admin/posts');
     }
 
     /**
@@ -107,5 +125,10 @@ class AdminPostsController extends Controller
     public function destroy($id)
     {
         //
+
+        $post = Post::findOrFail($id);
+        unlink(public_path().$post->photo->photo);
+        $post->delete();
+        return redirect('/admin/posts');
     }
 }
